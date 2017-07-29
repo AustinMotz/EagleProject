f1dbd1b2aacb0720a444e009bdb7c518eacf281e
johnmays <johnkmayonnaise@gmail.com>
2017-07-29 13:50:38 -0400

    Project One, no comments
    Only goalOne is commented in, blinking on an off every second.

---
+++ Project 1/TE-project-1-no-comments/TE-project-1-no-comments.ino
@@ -0,0 +1,63 @@
+
+//CONSULT COMMENTED VERSION FOR INSTRUCTIONS
+
+const int ledPin = 2;
+
+void setup() {
+  pinMode(ledPin, INPUT);
+
+}
+
+void loop() {
+  //goalOne();
+  //goalTwo();
+  //goalThree();
+  goalFour();
+
+}
+
+void goalOne(){
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
+ 
+void goalTwo(){
+  const int specifiedTime = 0;
+  
+  digitalWrite(ledPin, HIGH);
+  delay(specifiedTime);
+  digitalWrite(ledPin, LOW);
+  delay(specifiedTime);
+}
+
+void goalThree(){
+  const int specifiedTimeOne = 0;
+  const int specifiedTimeTwo = 0;
+  
+  digitalWrite(ledPin, HIGH);
+  delay(specifiedTimeOne);
+  digitalWrite(ledPin, LOW);
+  delay(specifiedTimeTwo);
+}
+
+void goalFour(){
+  const int specifiedTimeOne = 500;
+  const int specifiedTimeTwo = 40;
+  const int specifiedTimeThree = 100;
+  const int specifiedTimeFour = 2000;
+  
+  digitalWrite(ledPin, HIGH);
+  delay(specifiedTimeOne);
+  digitalWrite(ledPin, LOW);
+  delay(specifiedTimeTwo);
+  digitalWrite(ledPin, HIGH);
+  delay(specifiedTimeThree);
+  digitalWrite(ledPin, LOW);
+  delay(specifiedTimeFour);
+}
+
+
+
+
