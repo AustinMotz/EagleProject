1342ab644d8a539d01ea58e9bba7a956de1d6af0
Tass0sm <tassos.manganaris@gmail.com>
2017-08-30 16:52:02 -0400

    Add files via upload

---
+++ 10 - Color Sensor/TEproject10/TEproject10.ino
@@ -0,0 +1,122 @@
+#include <Adafruit_MotorShield.h>
+
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+Adafruit_DCMotor leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor rightMotor = AFMS.getMotor(2);
+
+bool hasStopped = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt
+  }  
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  // turn on motor
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+}
+
+
+void loop() {
+  displayColor();
+}
+
+
+void goalOne() {
+  uint16_t clear, red, green, blue;
+  tcs.setInterrupt(false);      // turn on LED
+  delay(2000);  // takes 50ms to read 
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+  
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+  Serial.println();
+
+  //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
+  // Comment this in for regular decimal RGB Values.
+}
+
+void goalTwo() {
+  uint16_t clear, red, green, blue;
+  tcs.setInterrupt(false);      // turn on LED
+  delay(2000);  // takes 50ms to read 
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+
+  if (red < 50 && g < 50 && b < 50) {
+    hasStopped = true;
+  }
+
+  if (hasStopped == true) { // "== true" is not technically necessary. Done to show equivilance to students.
+    leftMotor->setSpeed(0);
+    rightMotor->setSpeed(0);
+  } else {
+    leftMotor->setSpeed(150);
+    rightMotor->setSpeed(150);
+    }
+}
+
+void goalThree() {
+    uint16_t clear, red, green, blue;
+  tcs.setInterrupt(false);      // turn on LED
+  delay(2000);  // takes 50ms to read 
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+
+  if (red < 50 && g < 50 && b < 50) {
+    leftMotor->setSpeed(150);
+    rightMotor->setSpeed(0);
+  } else {
+    leftMotor->setSpeed(0);
+    rightMotor->setSpeed(150);
+  }
+}
+
+
